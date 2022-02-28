40db161528b40be763017cac3e5600b92714aec2
Jesus <jesus.joanaazuara@usp.ceu.es>
2022-02-28 16:49:48 +0100

    He hecho unas modificaciones

--- src/main/java/com/jesusjoana/recursividad/App.java
+++ src/main/java/com/jesusjoana/recursividad/App.java
@@ -35,18 +35,20 @@ public class App
     /**
 	    * Método recursivo que implementa el método de Montecarlo, se le pasa por parámetro el número de iteraciones
 	    * que vamos a realizar a la hora de calcular el número Pi
-	    * @param pasos Nº de iteraciones.
-	    * @return Pi.
+	    * @param numeroIteraciones Nº de iteraciones.
+	    * @return n
     */
     public static double puntoDentro(double numeroIteraciones){
 
+        //En esta variable se calcula el numero de veces qeu el punto al azar cae dentro del circulo
         double n = 0;
+        //En coordenadaX y en coordenadaY se almacenan las coordenadas de un punto generado aleatoriamente en un cuadrado centrado en el origen de lado 2
         double coordenadaX = 1 - 2 * Math.random();
         double coordenadaY = 1 - 2 * Math.random();
+        //En la variable distancia almacenamos la distancia desde el origen hasta este punto aleatorio
         double distancia = Math.sqrt(Math.pow(coordenadaX, 2) + Math.pow(coordenadaY, 2));
-        //System.out.println("CoordenadaX: " + coordenadaX + "Coordenada Y: " + coordenadaY);
-        //System.out.println("Distancia al origen: " +  distancia);
         
+        //Parte recursiva del metodo. La condicion de salida  de la recursividad es cuando se alcanza el numero de iteraciones preestablecidas
         if (numeroIteraciones > 0){
             if (distancia <= 1){
                 n = puntoDentro(--numeroIteraciones) + 1;
@@ -61,7 +63,7 @@ public class App
                 n = 0;
             }
         }
-        //System.out.println("El valor de n es:" + n);
+
         return n;
 	}
 }
